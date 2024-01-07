789a4b5490192effd805b59b4e51d69f4a7582c8
IT <sayyambutt317@gmail.com>
2024-01-07 05:22:38 +0500

    updated

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -37,13 +37,65 @@ const Hero = () => {
                   <i class="ri-mail-line"></i>Hire me
                 </button>
               </a>
-                {/*========== See Portfolio ==========*/}
+              {/*========== See Portfolio ==========*/}
               <a
                 href="#portfolio"
                 className="text-smallText font-[600] text-[16px] border-b border-solid border-smallTextColor"
               >
                 See Portfolio
               </a>
+              {/*========== Intro ParaGraph ==========*/}
+            </div>
+            <p
+              data-aos="fade-left"
+              data-aos-duration="1500"
+              className="flex gap-2 text-headingColor mt-14 font-[500] text-[15px] leading-7 sm:pl-14 sm:pr-10"
+            >
+              <span>
+                <i class="ri-apps-2-line"></i>
+              </span>
+              I am a qualified l Software developer and eager to start a new
+              career with Strong creative and analytical skills. I am eager to
+              contribute to a dynamic team and further enhance my skills in
+              software Development
+            </p>
+            {/*========== Social Logos ==========*/}
+            <div className="flex items-center gap-8 mt-14">
+              <span className="text-smallTextColor text-[15px] font-[600]">
+                Find me on
+              </span>
+              <span>
+                <a
+                  href="#linkedIn"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i class="ri-linkedin-box-fill"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  href="#Github"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                 <i class="ri-github-fill"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  href="#Instagram"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i class="ri-instagram-line"></i>
+                </a>
+              </span>
+              <span>
+                <a
+                  href="#Facebook"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <i class="ri-facebook-circle-fill"></i>
+                </a>
+              </span>
             </div>
           </div>
         </div>
