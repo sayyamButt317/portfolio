93c207d6c442e9590ea4ab1ef81d18a0c62003fa
Sayyam Butt <sayyambutt317@gmail.com>
2024-01-14 06:52:50 +0500

    saturday updated

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -22,8 +22,8 @@ const Hero = () => {
               className="text-headingColor font-[800] text-[1.8rem] sm:text-[40px]"
             >
               I'm Sayyam Butt
-              <br />
-              Software Developer
+              <br className="text-titleTextColor font-mono text-[1.8rem] sm:text-[45px]"/>
+              QuestBill World
             </h1>
             {/*==========Hire ME Button ==========*/}
             <div
@@ -54,14 +54,11 @@ const Hero = () => {
               <span>
                 <i class="ri-apps-2-line"></i>
               </span>
-              I am a qualified l Software developer and eager to start a new
-              career with Strong creative and analytical skills. I am eager to
-              contribute to a dynamic team and further enhance my skills in
-              software Development
+              Simplify your life with EasyBills, your go-to solution for managing and paying utility bills seamlessly. Say goodbye to the hassle of juggling multiple due dates and paper statements. Our user-friendly platform empowers you to take control of your expenses effortlessly.
             </p>
             {/*========== Social Logos ==========*/}
             <div className="flex items-center gap-8 mt-14">
-              <span className="text-smallTextColor text-[15px] font-[600]">
+              <span className="text-smallTextColor text-[1nppx] font-[500]">
                 Find me on
               </span>
               <span>
@@ -77,7 +74,7 @@ const Hero = () => {
                   href="#Github"
                   className="text-smallTextColor text-[18px] font-[600]"
                 >
-                 <i class="ri-github-fill"></i>
+                  <i class="ri-github-fill"></i>
                 </a>
               </span>
               <span>
@@ -96,6 +93,47 @@ const Hero = () => {
                   <i class="ri-facebook-circle-fill"></i>
                 </a>
               </span>
+            </div>
+          </div>
+          {/*====================Hero Image=====================*/}
+          <div className="md-basis-1/3 mt-10 sm:mt-0">
+            <figure className="flex items-center justify-center">
+              <img src={heroImg} alt=""></img>
+            </figure>
+          </div>
+          {/*====================Hero Content=====================*/}
+          <div className="md:basis-1/5 flex justify-betwen text-center mt-10 flex-wrap gap-3 md:mt-0 md:flex-col md:justify-end md:text-end">
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={3800} duration={2} suffix="+"></CountUp>
+              </h2>
+              <h4 className="text-headingColor font-[600] text-[18px]">
+                Active Users
+              </h4>
+            </div>
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={100} duration={2} suffix="%"></CountUp>
+              </h2>
+              <h4 className="text-headingColor font-[600] text-[18px]">
+                Success Rate
+              </h4>
+            </div>
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={230} duration={2} suffix="+"></CountUp>
+              </h2>
+              <h4 className="text-headingColor font-[600] text-[18px]">
+                Trusted Company
+              </h4>
+            </div>
+            <div className="mb-10">
+              <h2 className="text-headingColor font-[700] text-[32px]">
+                <CountUp start={0} end={300} duration={2}  prefix="$" suffix="M+"></CountUp>
+              </h2>
+              <h4 className="text-headingColor font-[600] text-[18px]">
+                Transaction
+              </h4>
             </div>
           </div>
         </div>
